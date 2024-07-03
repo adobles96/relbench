5556c0c60b61d66e93dd9d7cfa7d7820a2b484b2
adobles96 <alejandro.d.96@gmail.com>
2024-07-03 11:10:18 -0700

    Add tgn_node train script

---
+++ examples/tgn_node.py
@@ -0,0 +1,225 @@
+import argparse
+import copy
+import math
+import os
+from typing import Dict
+
+import numpy as np
+import torch
+from inferred_stypes import dataset2inferred_stypes
+from model import Model
+from text_embedder import GloveTextEmbedding
+from torch.nn import BCEWithLogitsLoss, L1Loss
+from torch_frame.config.text_embedder import TextEmbedderConfig
+from torch_geometric.loader import NeighborLoader
+from torch_geometric.seed import seed_everything
+from tqdm import tqdm
+
+from relbench.data import NodeTask, RelBenchDataset
+from relbench.data.task_base import TaskType
+from relbench.datasets import get_dataset
+from relbench.external.graph import get_node_train_table_input, make_fact_dimension_graph
+
+
+FACT_TABLES = {
+    "rel-amazon": ["review"],
+    "rel-hm": ["transactions"],
+}
+
+DIMENSION_TABLES = {
+    "rel-amazon": ["customer", "product"],
+    "rel-hm": ["customer", "article"],
+}
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", type=str, default="rel-event")
+parser.add_argument("--task", type=str, default="user-attendance")
+parser.add_argument("--lr", type=float, default=0.005)
+parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--batch_size", type=int, default=512)
+parser.add_argument("--channels", type=int, default=128)
+parser.add_argument("--aggr", type=str, default="sum")
+parser.add_argument("--num_layers", type=int, default=2)
+parser.add_argument("--num_neighbors", type=int, default=128)
+parser.add_argument("--temporal_strategy", type=str, default="last")  # changed from "uniform"
+parser.add_argument("--max_steps_per_epoch", type=int, default=2000)
+parser.add_argument("--num_workers", type=int, default=0)
+parser.add_argument("--seed", type=int, default=42)
+parser.add_argument(
+    "--cache_dir",
+    type=str,
+    default=os.path.expanduser("~/.cache/relbench/materialized"),
+)
+
+
+def train() -> float:
+    model.train()
+
+    loss_accum = count_accum = 0
+    steps = 0
+    total_steps = min(len(loader_dict["train"]), args.max_steps_per_epoch)
+    for batch in tqdm(loader_dict["train"], total=total_steps):
+        batch = batch.to(device)
+
+        optimizer.zero_grad()
+        pred = model(
+            batch,
+            task.entity_table,
+        )
+        pred = pred.view(-1) if pred.size(1) == 1 else pred
+
+        loss = loss_fn(pred.float(), batch[entity_table].y.float())
+        loss.backward()
+        optimizer.step()
+
+        loss_accum += loss.detach().item() * pred.size(0)
+        count_accum += pred.size(0)
+
+        steps += 1
+        if steps > args.max_steps_per_epoch:
+            break
+
+    return loss_accum / count_accum
+
+
+@torch.no_grad()
+def test(loader: NeighborLoader) -> np.ndarray:
+    model.eval()
+
+    pred_list = []
+    for batch in loader:
+        batch = batch.to(device)
+        pred = model(
+            batch,
+            task.entity_table,
+        )
+        if task.task_type == TaskType.REGRESSION:
+            assert clamp_min is not None
+            assert clamp_max is not None
+            pred = torch.clamp(pred, clamp_min, clamp_max)
+
+        if task.task_type in [
+            TaskType.BINARY_CLASSIFICATION,
+            TaskType.MULTILABEL_CLASSIFICATION,
+        ]:
+            pred = torch.sigmoid(pred)
+
+        pred = pred.view(-1) if pred.size(1) == 1 else pred
+        pred_list.append(pred.detach().cpu())
+    return torch.cat(pred_list, dim=0).numpy()
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        torch.set_num_threads(1)
+    seed_everything(args.seed)
+
+    dataset: RelBenchDataset = get_dataset(name=args.dataset, process=False)
+    task: NodeTask = dataset.get_task(args.task, process=True)
+
+    col_to_stype_dict = dataset2inferred_stypes[args.dataset]
+
+    # data, col_stats_dict = make_pkey_fkey_graph(
+    #     dataset.db,
+    #     col_to_stype_dict=col_to_stype_dict,
+    #     text_embedder_cfg=TextEmbedderConfig(
+    #         text_embedder=GloveTextEmbedding(device=device), batch_size=256
+    #     ),
+    #     cache_dir=os.path.join(args.cache_dir, args.dataset),
+    # )
+    data, col_stats_dict = make_fact_dimension_graph(
+        dataset.db,
+        fact_tables=FACT_TABLES[args.dataset],
+        dimension_tables=DIMENSION_TABLES[args.dataset],
+        col_to_stype_dict=col_to_stype_dict,
+        text_embedder_cfg=TextEmbedderConfig(
+            text_embedder=GloveTextEmbedding(device=device), batch_size=256
+        ),
+        cache_dir=os.path.join(args.cache_dir, args.dataset),
+    )
+
+    clamp_min, clamp_max = None, None
+    if task.task_type == TaskType.BINARY_CLASSIFICATION:
+        out_channels = 1
+        loss_fn = BCEWithLogitsLoss()
+        tune_metric = "roc_auc"
+        higher_is_better = True
+        multilabel = False
+    elif task.task_type == TaskType.REGRESSION:
+        out_channels = 1
+        loss_fn = L1Loss()
+        tune_metric = "mae"
+        higher_is_better = False
+        # Get the clamp value at inference time
+        clamp_min, clamp_max = np.percentile(
+            task.train_table.df[task.target_col].to_numpy(), [2, 98]
+        )
+        multilabel = False
+    elif task.task_type == TaskType.MULTILABEL_CLASSIFICATION:
+        out_channels = task.num_labels
+        loss_fn = BCEWithLogitsLoss()
+        tune_metric = "multilabel_auprc_macro"
+        higher_is_better = True
+        multilabel = True
+    else:
+        raise ValueError(f"Task type {task.task_type} is unsupported")
+
+    loader_dict: Dict[str, NeighborLoader] = {}
+    for split, table in [
+        ("train", task.train_table),
+        ("val", task.val_table),
+        ("test", task.test_table),
+    ]:
+        table_input = get_node_train_table_input(
+            table=table, task=task, multilabel=multilabel
+        )
+        entity_table = table_input.nodes[0]
+        loader_dict[split] = NeighborLoader(
+            data,
+            num_neighbors=[int(args.num_neighbors / 2**i) for i in range(args.num_layers)],
+            time_attr="time",
+            input_nodes=table_input.nodes,
+            # input_time=table_input.time,  # overrides time_attr
+            transform=table_input.transform,
+            batch_size=args.batch_size,
+            temporal_strategy=args.temporal_strategy,
+            shuffle=split == "train",
+            num_workers=args.num_workers,
+            persistent_workers=args.num_workers > 0,
+        )
+
+    model = Model(
+        data=data,
+        col_stats_dict=col_stats_dict,
+        num_layers=args.num_layers,
+        channels=args.channels,
+        out_channels=out_channels,
+        aggr=args.aggr,
+        norm="batch_norm",
+    ).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    state_dict = None
+    best_val_metric = -math.inf if higher_is_better else math.inf
+    for epoch in range(1, args.epochs + 1):
+        train_loss = train()
+        val_pred = test(loader_dict["val"])
+        val_metrics = task.evaluate(val_pred, task.val_table)
+        print(f"Epoch: {epoch:02d}, Train loss: {train_loss}, Val metrics: {val_metrics}")
+
+        if (higher_is_better and val_metrics[tune_metric] > best_val_metric) or (
+            not higher_is_better and val_metrics[tune_metric] < best_val_metric
+        ):
+            best_val_metric = val_metrics[tune_metric]
+            state_dict = copy.deepcopy(model.state_dict())
+
+    model.load_state_dict(state_dict)
+    val_pred = test(loader_dict["val"])
+    val_metrics = task.evaluate(val_pred, task.val_table)
+    print(f"Best Val metrics: {val_metrics}")
+
+    test_pred = test(loader_dict["test"])
+    test_metrics = task.evaluate(test_pred)
+    print(f"Best test metrics: {test_metrics}")
